1bcbe966abb5511e98ed8a652341367441fcb330
Johnny <jony371400@gmail.com>
2022-10-10 22:12:26 +0800

    20221010-2205

--- my-app1/src/App.js
+++ my-app1/src/App.js
@@ -25,18 +25,12 @@ function App() {
             <Route path="users">
               <Route index element={<List />} />
               <Route path=":userId" element={<Single />} />
-              <Route
-                path="new"
-                element={<New inputs={userInputs} title="Add New User" />}
-              />
+              <Route path="new"     element={<New inputs={userInputs} title="Add New User" />}/>
             </Route>
             <Route path="products">
               <Route index element={<List />} />
               <Route path=":productId" element={<Single />} />
-              <Route
-                path="new"
-                element={<New inputs={productInputs} title="Add New Product" />}
-              />
+              <Route path="new"        element={<New inputs={productInputs} title="Add New Product" />}/>
             </Route>
           </Route>
         </Routes>
