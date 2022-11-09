2a87ab6a8d43e45370df5d5938bbf4a8b8cc8d1c
Johnny <jony371400@gmail.com>
2022-11-09 22:15:55 +0800

    20221108-2215

--- my-app2/src/component/PageDashboard/d1Contain.jsx
+++ my-app2/src/component/PageDashboard/d1Contain.jsx
@@ -1,12 +1,44 @@
-
+import React, { useState } from 'react';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 
-const d1Contain = () => {
+const D1Contain = () => {
+    // const [isId, setIsId] = useState(0);
+    const [isName, setIsName] = useState([]);
+
+    // const ShowText = (props) => {
+    //     return <p>Click {props.value}</p>;
+    // };
+
+    let jsonData
+
+    const FetchClick = () => {
+        console.log('HIHIHI')
+
+        fetch('http://127.0.0.1:5000')
+            .then((response) => response.json())
+            .then((json) => {
+                jsonData = json
+                console.log(jsonData)
+                // console.log(typeof (jsonData))
+                // console.log(jsonData.Id)
+                // console.log(jsonData.Name)
+                UpdateUI()
+            })
+            .catch((error) => console.log(error));
+    }
+
+    const UpdateUI = () => {
+        // setIsId(jsonData.Id)
+        setIsName(jsonData.Name)
+    }
 
     return (
         <div className="d1Contain">
             <DashboardIcon className="d1icon" />
+            <button onClick={FetchClick}>Fetch</button>
+            {/* <a >{setIsId}</a> */}
+            <p>{setIsName}</p>
         </div>
     )
 }
-export default d1Contain;
+export default D1Contain;
